a18e8965c3687d0f7b09e701856b48568add25ee
Alex Vzorov <alex.vzorov@gmail.com>
2021-12-05 13:39:04 +0800

    Solve D4/2

--- 2021/day/4/Main.hs
+++ 2021/day/4/Main.hs
@@ -5,6 +5,7 @@ module Main where
 
 import Data.Either (isLeft, isRight)
 import Data.Foldable (find, foldl')
+import Data.Maybe (maybeToList)
 import Data.Monoid (Sum (getSum))
 import System.Environment (getArgs)
 
@@ -13,19 +14,46 @@ type Row = (Int, T [Either Int Int])
 type State = [Row]
 type Board = [[Int]]
 
-ans1 [] state = Nothing
-ans1 (i : is) state =
+type Solution = [Int] -> State -> Maybe Int
+
+ans1 :: Solution
+ans1 input state =
+  case winnerScores input state of
+    [] -> Nothing
+    xs -> Just (head xs)
+
+ans2 :: Solution
+ans2 input state =
+  case winnerScores input state of
+    [] -> Nothing
+    xs -> Just (last xs)
+
+-- >>> winnerScores input boards
+-- [4512,2192,1924]
+
+winnerScores :: [Int] -> State -> [Int]
+winnerScores [] state = []
+winnerScores _ [] = []
+winnerScores (i : is) state =
   let state' = draw i state
-   in case winner state' of
-        Nothing ->
-          ans1 is state'
-        Just w ->
-          let rows = lookupWinner w state'
-           in Just (i * sum (concatMap unmarkedNumbers rows))
+   in case winners state' of
+        [] -> winnerScores is state'
+        ws ->
+          let state'' = filter (\(board, _) -> board `notElem` ws) state'
+           in concatMap (\w -> maybeToList (finalScore i w state')) ws ++ winnerScores is state''
  where
+  finalScore :: Int -> Int -> [Row] -> Maybe Int
+  finalScore i w state =
+    let rows = lookupWinner w state
+     in case concatMap unmarkedNumbers rows of
+          [] -> Nothing
+          xs -> Just (i * sum xs)
+
+  unmarkedNumbers :: Row -> [Int]
   unmarkedNumbers (_, R xs) = concatMap onlyLeft xs
   unmarkedNumbers _ = []
 
+  onlyLeft :: Either b a -> [b]
   onlyLeft (Left i) = [i]
   onlyLeft _ = []
 
@@ -47,12 +75,8 @@ instance Draw Row where
 instance Draw State where
   draw n xs = fmap (draw n) xs
 
--- >>> wins (1, C [Right 0, Right 24, Right 7, Right 5, Right 19])
--- True
--- >>> wins (1, C [Left 0, Left 24, Left 7, Left 5, Left 19])
--- False
-winner :: State -> Maybe Int
-winner rs = fst <$> find wins rs
+winners :: State -> [Int]
+winners rs = fst <$> filter wins rs
  where
   wins (_, C xs) = all isRight xs
   wins (_, R xs) = all isRight xs
@@ -71,8 +95,13 @@ main = do
       -- print input
       -- mapM_ print boards
       print (ans1 input boards)
+    ["2"] -> do
+      (input, boards) <- readInput
+      -- print input
+      -- mapM_ print boards
+      print (ans2 input boards)
     _ ->
-      print ("Usage: cabal run day4 1 < input.txt")
+      print ("Usage: cabal run day4 1|2 < input.txt")
 
 ------------------------ Input Parsers -----------------------------------------
 readInput :: IO ([Int], State)
@@ -120,9 +149,7 @@ transpose5 e = error ("invalid input: " <> show e)
 ------------------------ Tests -------------------------------------------------
 -- >>> test
 -- True
-test =
-  let state = compileBoard 1 board1 <> compileBoard 2 board2 <> compileBoard 3 board3
-   in (ans1 input state) == Just 4512
+test = ans1 input boards == Just 4512
 
 board1, board2, board3 :: Board
 board1 =
@@ -146,6 +173,9 @@ board3 =
   , [22, 11, 13, 6, 5]
   , [2, 0, 12, 3, 7]
   ]
+
+boards :: State
+boards = compileBoard 1 board1 <> compileBoard 2 board2 <> compileBoard 3 board3
 
 input :: [Int]
 input = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1]
