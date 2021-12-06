c77b4cd084e4573d21919337029609c5887b35ed
Alex Vzorov <alex.vzorov@gmail.com>
2021-12-06 16:01:45 +0800

    Solve Day 6, Part I and II, in Rust

---
+++ 2021/examples/day6.rs
@@ -0,0 +1,46 @@
+use std::io::{self, BufRead};
+use std::str::FromStr;
+
+fn step(state: &mut [&mut u64; 9]) {
+    let new_spawns = &state[0].clone();
+    for i in 0..8 {
+        *state[i] = *state[i + 1];
+    }
+    *state[6] += new_spawns.clone();
+    *state[8] = new_spawns.clone();
+}
+
+fn read_integers() -> Vec<usize> {
+    let mut vec = Vec::new();
+
+    for line in io::stdin().lock().lines() {
+        for str in line.unwrap().split(',') {
+            vec.push(usize::from_str(str).unwrap());
+        }
+    }
+    vec
+}
+
+fn main() {
+    let state = &mut [
+        &mut 0u64, &mut 0, &mut 0, &mut 0, &mut 0, &mut 0, &mut 0, &mut 0, &mut 0,
+    ];
+    let input = read_integers();
+
+    // initialize
+    input.iter().for_each(|i| *state[*i] += 1);
+
+    // simulate
+    for _ in 1..=80 {
+        step(state);
+        // println!("{} state: {:?}", i, state);
+    }
+
+    println!("Answer 1: {}", state.iter().map(|i| **i).sum::<u64>());
+
+    for _ in 81..=256 {
+        step(state);
+    }
+
+    println!("Answer 2: {}", state.iter().map(|i| **i).sum::<u64>());
+}
